84b7ddddd5e02c239a5b086d65664c7a18751541
mizutani <mizutani-k@pise.co.jp>
2017-01-18 17:56:56 +0900

    試験中

--- QueueTriggerNodeJS2/index.js
+++ QueueTriggerNodeJS2/index.js
@@ -5,7 +5,7 @@ function post_line(event)
 {
     var post_data = JSON.stringify({
         "replyToken" : event.replyToken,
-        "messages"   : [ "hogehoge" ]
+        "messages"   : [ event.message ]
     });
     var parse_url = url.parse("https://api.line.me/v2/bot/message/reply");
     var post_options = {
